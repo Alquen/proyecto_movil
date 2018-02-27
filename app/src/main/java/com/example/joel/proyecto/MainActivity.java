b5e18700a6c85434718af23442311414caeb2415
Alquen <alquen@outlook.com>
2018-02-26 21:00:42 -0600

    Diseno21212 fffff Signed-off-by: Alquen <alquen@outlook.com>
    Signed-off-by: Alquen <alquen@outlook.com>

--- app/src/main/java/com/example/joel/proyecto/MainActivity.java
+++ app/src/main/java/com/example/joel/proyecto/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.joel.proyecto;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,6 +12,23 @@ Button botonregistro;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnregistro = (Button) findViewById(R.id.btnregistrar);
+        btnregistro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,RegistroUsuario.class);
+                startActivity(intent);
+            }
+        });
+
+
+
+
+
+
+
+
 
     }
 }
